1404a86586b4d30aa25e1fb0b26dadb43a608d6d
Finn Voichick <fvoichick@gmail.com>
2015-08-21 17:16:49 -0500

    Removed unnecessary comments

--- src/org/usfirst/frc/team1306/robot/RobotMap.java
+++ src/org/usfirst/frc/team1306/robot/RobotMap.java
@@ -22,15 +22,6 @@ import edu.wpi.first.wpilibj.interfaces.Accelerometer;
  * floating around.
  */
 public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
 
 	// Drive train motors
 	public static SpeedController driveFrontLeft;
@@ -48,7 +39,7 @@ public class RobotMap {
 	public static DigitalInput toteSwitch;
 	public static Gyro gyro;
 	public static Accelerometer accelerometer;
-	public static AnalogInput rangeFinder;
+	public static AnalogInput rangefinder;
 	public static Encoder driveFrontLeftEncoder;
 	public static Encoder driveRearLeftEncoder;
 	public static Encoder driveFrontRightEncoder;
@@ -66,13 +57,6 @@ public class RobotMap {
 	public static Elevator elevator;
 
 	static void init() {
-		/*
-		 * DRIVE_FRONT_LEFT = new PIDMotor(new Talon(0),
-		 * DRIVE_FRONT_LEFT_ENCODER); DRIVE_REAR_LEFT = new PIDMotor(new
-		 * Talon(1), DRIVE_REAR_LEFT_ENCODER); DRIVE_FRONT_RIGHT = new
-		 * PIDMotor(new Talon(2), DRIVE_FRONT_RIGHT_ENCODER); DRIVE_REAR_RIGHT =
-		 * new PIDMotor(new Talon(3), DRIVE_REAR_RIGHT_ENCODER);
-		 */
 
 		driveFrontLeft = new Talon(1);
 		driveRearLeft = new Talon(3);
@@ -93,7 +77,7 @@ public class RobotMap {
 		toteSwitch = new DigitalInput(2);
 		gyro = new Gyro(0);
 		accelerometer = new BuiltInAccelerometer();
-		rangeFinder = new AnalogInput(1);
+		rangefinder = new AnalogInput(1);
 
 		elevatorMotor = new Talon(4);
 		elevatorEncoder = new Encoder(18, 19);
