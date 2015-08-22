c03bcd1955eb5f9e59f5b83123778b98cf76b9ff
James Tautges <jamesdtautges@gmail.com>
2015-08-22 14:19:56 -0500

    Added prints.

--- src/org/usfirst/frc/team1306/robot/commands/MonitorRobot.java
+++ src/org/usfirst/frc/team1306/robot/commands/MonitorRobot.java
@@ -22,6 +22,8 @@ public class MonitorRobot extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	SmartDashboard.putNumber("Elevator Height", RobotMap.elevatorEncoder.getDistance());
+    	SmartDashboard.putNumber("Dart Position", RobotMap.grabberMotor.getPosition());
+    	SmartDashboard.putNumber("Dart Setpoint", RobotMap.grabberMotor.getSetpoint());
     }
 
     // Make this return true when this Command no longer needs to run execute()
